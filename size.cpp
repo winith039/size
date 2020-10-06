c529b295d4a1addbb9fb0d6c1f2766cbcef0ec8a
winith039 <65061103+winith039@users.noreply.github.com>
2020-10-06 16:58:33 +0530

    created

---
+++ size.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int i;
+	char c;
+	float f;
+	double d;
+	
+	cout<<"size of char "<<sizeof(c)<<" bytes\n"
+	    <<"size of int "<<sizeof(i)<<" bytes\n"
+		<<"size of float "<<sizeof(f)<<" bytes\n"
+		<<"size of double "<<sizeof(d)<<" bytes";	}
